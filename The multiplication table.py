4ae6bef7030fc49955b30094649ad77071283770
Exmanq <116520260+Frai30100@users.noreply.github.com>
2023-10-24 21:00:00 +0300

    Add files via upload

---
+++ The multiplication table.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a number for the multiplication table: "))
+print("Here is the multiplication table for: ",number)
+for i in range(1,11):
+    result = number * i
+    print(number, "x", i, "=", result)
+
+# Prompt the user for an integer.
+# Display a header to let the user know that this is a multiplication table for the entered number.
+# Create a loop that iterates from 1 to 11.
+# Within the loop, establish a variable to store the multiplication result and calculate this result.
+# Present the result in an aesthetically pleasing format to create a well-structured multiplication table.
+# Created by Frai30100
